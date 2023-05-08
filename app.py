93485341b01ada7b97923a4ecb20a82ccb01a928
Kalavala <rkalavala@saintpeters.edu>
2023-05-08 18:02:10 -0400

    Posting all the project requirements online

---
+++ app.py
@@ -0,0 +1,19 @@
+
+##Imporing the required libraries 
+import streamlit as st'
+import pandas as pd
+import numpy as np
+
+## Calling the important functions
+
+
+
+## start the streamlit app 
+def app():
+    ##this is the form we should put togther 
+    return 200
+
+
+## Executing the app 
+if __name__ == "__main__"
+    app()
